9abc76150cc9ae4ec860d4c06b51b6366544f478
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-04-16 13:54:02 +0900

    [Bronze V 1000] Title: A+B, Time: 68 ms, Memory: 5516 KB -NKLCBHub

---
+++ #1000 A+B/A＋B.cs
@@ -0,0 +1,22 @@
+   internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] nums = Console.ReadLine().Split();
+
+
+            int A = int.Parse(nums[0]);
+            int B = int.Parse(nums[1]);
+
+
+            Console.WriteLine(Sum(A, B));
+
+            int Sum(int a, int b)
+            {
+                return a + b;
+            }
+
+        }
+
+    }
