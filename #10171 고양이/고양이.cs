457d2e486d0e81619a7b905c65033c71033495b9
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-04-18 11:14:43 +0900

    [Bronze V 10171] Title: 고양이, Time: 60 ms, Memory: 5324 KB -NKLCBHub

---
+++ #10171 고양이/고양이.cs
@@ -0,0 +1,11 @@
+ internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
