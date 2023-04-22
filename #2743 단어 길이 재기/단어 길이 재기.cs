579977d716dcdfc9de07c7764d6ae6a8cbc00790
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-04-23 00:05:34 +0900

    [Bronze V 2743] Title: 단어 길이 재기, Time: 64 ms, Memory: 5592 KB -NKLCBHub

---
+++ #2743 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,11 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           string s= Console.ReadLine();
+            Console.WriteLine(s.Count());
+
+        }
+
+    }
