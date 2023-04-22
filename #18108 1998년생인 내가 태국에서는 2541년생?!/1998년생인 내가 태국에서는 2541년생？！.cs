1e7396591d7236214ae6d3f0b9584eb1954e4d08
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-04-23 00:04:30 +0900

    [Bronze V 18108] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5488 KB -NKLCBHub

---
+++ #18108 1998년생인 내가 태국에서는 2541년생?!/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,11 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           string Y= Console.ReadLine();
+          int Koy =Convert.ToInt32(Y)-543;
+            Console.WriteLine(Koy);
+        }
+
+    }
