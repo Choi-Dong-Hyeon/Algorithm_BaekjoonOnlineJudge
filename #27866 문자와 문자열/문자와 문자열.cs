9262d7061a19a0f09964ba2aab410dddca7ae573
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-04 19:54:35 +0900

    [Bronze V 27866] Title: 문자와 문자열, Time: 60 ms, Memory: 5504 KB -NKLCBHub

---
+++ #27866 문자와 문자열/문자와 문자열.cs
@@ -0,0 +1,10 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           string w= Console.ReadLine();
+           int c=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(w[c-1]);
+        }
+    }
