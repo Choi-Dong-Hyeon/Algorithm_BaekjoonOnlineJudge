2e39e518b0d3ea2e75aa7fcfa7cd04f79e8e4af1
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-14 02:19:12 +0900

    [Bronze V 11942] Title: 고려대는 사랑입니다, Time: 52 ms, Memory: 5316 KB -NKLCBHub

---
+++ #11942 고려대는 사랑입니다/고려대는 사랑입니다.cs
@@ -0,0 +1,17 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Show show = new Show();
+        }
+    }
+
+    class Show
+    {
+        public Show()
+        {
+            Console.WriteLine("고려대학교");
+
+        }
+    }
