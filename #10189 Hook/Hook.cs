8462a9d218a2e6679c815550e1040f5a4f0981da
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-14 02:19:04 +0900

    [Bronze V 10189] Title: Hook, Time: 60 ms, Memory: 5332 KB -NKLCBHub

---
+++ #10189 Hook/Hook.cs
@@ -0,0 +1,19 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Show show = new Show();
+        }
+    }
+
+    class Show
+    {
+        public Show()
+        {
+            Console.WriteLine("#  # #### #### #  #");
+            Console.WriteLine("#### #  # #  # # #");
+            Console.WriteLine("#### #  # #  # # #");
+            Console.WriteLine("#  # #### #### #  #");
+        }
+    }
---
+++ #10189 Hook/README.md
@@ -0,0 +1,15 @@
+# <img width="20px"  src="https://d2gd6pc034wcta.cloudfront.net/tier/1.svg" class="solvedac-tier"> [Hook](https://www.acmicpc.net/problem/10189) 
+
+| 제출 번호 | 닉네임 | 채점 결과 | 메모리 | 시간 | 언어 | 코드 길이 |
+|---|---|---|---|---|---|---|
+|60730589|dong_hyeon|맞았습니다!! |5332KB|60ms|C#|419B|
+
+## 문제
+<p>Print out the word Hook as shown below.</p>
+
+## 입력
+
+
+## 출력
+<p>Print out the word Hook as shown below.</p>
+
