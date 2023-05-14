b0ce41d113f21242fca8508419fc91ef8662b7f1
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-15 00:16:14 +0900

    [Bronze V 5339] Title: 콜센터, Time: 56 ms, Memory: 5320 KB -NKLCBHub

---
+++ #5339 콜센터/콜센터.cs
@@ -0,0 +1,25 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+
+            Show show = new Show();
+        }
+    }
+
+    class Show
+    {
+        public Show()
+        {
+            Console.WriteLine("     /~\\");
+            Console.WriteLine("    ( oo|");
+            Console.WriteLine("    _\\=/_");
+            Console.WriteLine("   /  _  \\");
+            Console.WriteLine("  //|/.\\|\\\\");
+            Console.WriteLine(" ||  \\ /  ||");
+            Console.WriteLine("============");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+        }
+    }
