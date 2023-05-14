607213c746037a137b78094d8c28c18668b5b755
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-15 00:21:58 +0900

    [Bronze V 1271] Title: 엄청난 부자2, Time: 68 ms, Memory: 5680 KB -NKLCBHub

---
+++ #1271 엄청난 부자2/엄청난 부자2.cs
@@ -0,0 +1,29 @@
+using System.Numerics;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] nums = Console.ReadLine().Split();
+           
+
+            BigInteger num1 = BigInteger.Parse(nums[0]);
+            BigInteger num2 = BigInteger.Parse(nums[1]);
+
+            
+            
+
+            Show show = new Show(num1, num2);
+        }
+    }
+
+    class Show
+    {
+        public Show(BigInteger a, BigInteger b)
+        {
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+     
+
+        }
+    }
