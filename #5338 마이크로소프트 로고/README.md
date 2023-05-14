d620915b5a5356642733a4e45e960d34639565e9
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-15 00:18:57 +0900

    [Bronze V 5338] Title: 마이크로소프트 로고, Time: 60 ms, Memory: 5328 KB -NKLCBHub

---
+++ #5338 마이크로소프트 로고/README.md
@@ -0,0 +1,17 @@
+# <img width="20px"  src="https://d2gd6pc034wcta.cloudfront.net/tier/1.svg" class="solvedac-tier"> [마이크로소프트 로고](https://www.acmicpc.net/problem/5338) 
+
+| 제출 번호 | 닉네임 | 채점 결과 | 메모리 | 시간 | 언어 | 코드 길이 |
+|---|---|---|---|---|---|---|
+|60732275|dong_hyeon|맞았습니다!! |5328KB|60ms|C#|461B|
+
+## 문제
+<p>
+	마이크로소프트 로고를 예제 출력처럼 출력하는 프로그램을 작성하시오.</p>
+
+## 입력
+
+
+## 출력
+<p>
+	마이크로소프트 로고를 예제 출력처럼 출력한다.</p>
+
---
+++ #5338 마이크로소프트 로고/마이크로소프트 로고.cs
@@ -0,0 +1,20 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+
+            Show show = new Show();
+        }
+    }
+
+    class Show
+    {
+        public Show()
+        {
+            Console.WriteLine("       _.-;;-._");
+            Console.WriteLine("'-..-'|   ||   |");
+            Console.WriteLine("'-..-'|_.-;;-._|");
+            Console.WriteLine("'-..-'|   ||   |");
+            Console.WriteLine("'-..-'|_.-''-._|");
+        }
+    }
