231ccaba2eb93693ab3e27c29d1a9f65b9eb5d4d
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-15 00:16:21 +0900

    [Bronze V 5337] Title: 웰컴, Time: 52 ms, Memory: 5336 KB -NKLCBHub

---
+++ #5337 웰컴/웰컴.cs
@@ -0,0 +1,19 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+
+            Show show = new Show();
+        }
+    }
+
+    class Show
+    {
+        public Show()
+        {
+            Console.WriteLine(".  .   .");
+            Console.WriteLine("|  | _ | _. _ ._ _  _");
+            Console.WriteLine("|/\\|(/.|(_.(_)[ | )(/.");
+       
+        }
+    }
