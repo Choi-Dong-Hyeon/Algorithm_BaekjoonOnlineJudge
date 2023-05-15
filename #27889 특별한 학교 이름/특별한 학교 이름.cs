4442d4d4656d3d761d59b21281e9cc8cda640bb7
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-16 01:32:13 +0900

    [Bronze V 27889] Title: 특별한 학교 이름, Time: 72 ms, Memory: 5376 KB -NKLCBHub

---
+++ #27889 특별한 학교 이름/특별한 학교 이름.cs
@@ -0,0 +1,26 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+
+            switch (name)
+            {
+                case "NLCS":
+                    Console.WriteLine("North London Collegiate School");
+                    break;
+                case "BHA":
+                    Console.WriteLine("Branksome Hall Asia");
+                    break;
+                case "KIS":
+                    Console.WriteLine("Korea International School");
+                    break;
+                case "SJA":
+                    Console.WriteLine("St. Johnsbury Academy");
+                    break;
+            }
+
+
+        }
+    }
