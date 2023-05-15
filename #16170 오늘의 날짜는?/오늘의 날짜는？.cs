9b57d9969975652b6fd8afa54ce3d4ca0ab09040
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-16 01:32:07 +0900

    [Bronze V 16170] Title: 오늘의 날짜는?, Time: 56 ms, Memory: 5488 KB -NKLCBHub

---
+++ #16170 오늘의 날짜는?/오늘의 날짜는？.cs
@@ -0,0 +1,12 @@
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            
+
+
+        }
+    }
