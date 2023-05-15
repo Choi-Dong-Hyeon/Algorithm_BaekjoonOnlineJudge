6507d59c89f39a17ee46775ee97e068725f7fab1
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-16 01:32:33 +0900

    [Bronze IV 2440] Title: 별 찍기 - 3, Time: 296 ms, Memory: 6628 KB -NKLCBHub

---
+++ #2440 별 찍기 - 3/별 찍기 － 3.cs
@@ -0,0 +1,20 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = num; j >= i; j--)
+                {
+                    Console.Write("*");
+                }
+          
+                Console.WriteLine();
+
+            }
+
+        }
+    }
