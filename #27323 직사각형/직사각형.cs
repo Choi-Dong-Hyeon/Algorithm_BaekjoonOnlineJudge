85eb27b2c21c41240061f93e9490bf3e035f2a2e
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-16 01:32:58 +0900

    [Bronze V 27323] Title: 직사각형, Time: 60 ms, Memory: 5492 KB -NKLCBHub

---
+++ #27323 직사각형/직사각형.cs
@@ -0,0 +1,11 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int a= Convert.ToInt32(Console.ReadLine());
+           int b= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a*b);
+
+        }
+
+    }
