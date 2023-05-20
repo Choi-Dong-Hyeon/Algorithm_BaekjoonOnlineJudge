808069d9d1c742f50ec5588f9818b532302b4b59
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-21 02:57:44 +0900

    [Bronze V 25311] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 64 ms, Memory: 5372 KB -NKLCBHub

---
+++ #25311 UCPC에서 가장 쉬운 문제 번호는?/UCPC에서 가장 쉬운 문제 번호는？.cs
@@ -0,0 +1,18 @@
+using System;
+
+internal class Program
+{ 
+
+    static void Main(string[] args)
+    {
+        if (Console.ReadLine() != null)
+        {
+        Console.WriteLine('A');
+
+        }
+
+    }
+
+}
+
+
