d4bd5a1636889d0eda31c921d3f61a5dc4bbb8ea
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-05-23 01:14:50 +0900

    [Bronze V 14645] Title: 와이버스 부릉부릉, Time: 64 ms, Memory: 5632 KB -NKLCBHub

---
+++ #14645 와이버스 부릉부릉/와이버스 부릉부릉.cs
@@ -0,0 +1,20 @@
+using System;
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nums = Console.ReadLine().Split();
+            int num1 = Convert.ToInt32(nums[0]);
+            int num2 = Convert.ToInt32(nums[1]);
+
+            for(int i=0; i<num1; i++)
+            {
+                Console.ReadLine();
+            }
+            Console.WriteLine("비와이");
+
+        }
+    }
+
