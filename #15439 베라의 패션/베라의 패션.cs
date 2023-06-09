95b8293c6491050b46c15328333628ac2cd1fd1e
DongHyeon Choi <MicroSoftMainUser@Gmail.com>
2023-06-09 14:48:23 +0900

    [Bronze IV 15439] Title: 베라의 패션, Time: 72 ms, Memory: 5496 KB -NKLCBHub

---
+++ #15439 베라의 패션/베라의 패션.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        int targetnum = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine((targetnum-1)*targetnum);
+    }
+}
+
